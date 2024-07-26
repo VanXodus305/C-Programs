20a0bbb89c10e91241a2426e1a336d485f769165
Soumyadeep Kundu <soumyadeep.30505@gmail.com>
2024-07-27 00:04:44 +0530

    Added Question - 3 to Day - 2

---
+++ 2nd Year/Day - 2/Question - 3.c
@@ -0,0 +1,57 @@
+// WAP to represent a given sparse matrix in 3-tuple format using 2-D array.
+
+#include <stdio.h>
+
+void main()
+{
+  int m, n;
+  printf("Enter the size of the matrix: ");
+  scanf("%d %d", &m, &n);
+  int matrix[m][n];
+  printf("Enter the elements of the matrix:\n");
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      scanf("%d", &matrix[i][j]);
+    }
+  }
+  int count = 0;
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      if (matrix[i][j] != 0)
+      {
+        count++;
+      }
+    }
+  }
+  int sparseMatrix[count + 1][3];
+  sparseMatrix[0][0] = m;
+  sparseMatrix[0][1] = n;
+  sparseMatrix[0][2] = count;
+  count = 1;
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      if (matrix[i][j] != 0)
+      {
+        sparseMatrix[count][0] = i;
+        sparseMatrix[count][1] = j;
+        sparseMatrix[count][2] = matrix[i][j];
+        count++;
+      }
+    }
+  }
+  printf("3-tuple format of the given sparse matrix:\n");
+  for (int i = 0; i < count; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      printf("%d ", sparseMatrix[i][j]);
+    }
+    printf("\n");
+  }
+}
--- 2nd Year/Day - 2/README.md
+++ 2nd Year/Day - 2/README.md
@@ -2,15 +2,19 @@
 >
 > **WAP to create a 1-D array of n elements and perform the following menu based operations using function.**
 >
-> a. insert a given element at specific position.
-> b. delete an element from a specific position of the array.
-> c. linear search to search an element
-> d. traversal of the array
+> 1. insert a given element at specific position.
+> 2. delete an element from a specific position of the array.
+> 3. linear search to search an element.
+> 4. traversal of the array.
 
 > ### Question - 2
 >
 > **Write a program to perform the following operations on a given square matrix using functions:**
 >
-> i. Find the no.of nonzero element
-> ii. Display upper triangular matrix
-> iii. Display the elements of just above and below the main diagonal
+> 1. Find the no.of nonzero element
+> 2. Display upper triangular matrix
+> 3. Display the elements of just above and below the main diagonal
+
+> ### Question - 3
+>
+> **WAP to represent a given sparse matrix in 3-tuple format using 2-D array.**
