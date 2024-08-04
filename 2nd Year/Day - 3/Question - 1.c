2f484a23dd0ede4217df76fe7dfd62fca59a2f5b
Soumyadeep Kundu <soumyadeep.30505@gmail.com>
2024-08-04 17:12:09 +0530

    Updated Day - 3 Programs

--- 2nd Year/Day - 3/Question - 1.c
+++ 2nd Year/Day - 3/Question - 1.c
@@ -5,22 +5,24 @@
 
 void main()
 {
-  int temp, rows;
-  printf("Enter the number of number of rows in the sparse matrix: ");
-  scanf("%d", &rows);
-  int matrix[rows][3];
+  int **matrix = (int **)malloc(sizeof(int *)), temp;
+  matrix[0] = (int *)malloc(3 * sizeof(int));
   printf("Enter the sparse matrix in 3-tuple format: \n");
-  for (int i = 0; i < rows; i++)
+  for (int i = 0; i < 3; i++)
+    scanf("%d", matrix[0] + i);
+  matrix = (int **)realloc(matrix, (matrix[0][2] + 1) * sizeof(int *));
+  for (int i = 1; i < matrix[0][2] + 1; i++)
   {
+    matrix[i] = (int *)malloc(3 * sizeof(int));
     for (int j = 0; j < 3; j++)
     {
-      scanf("%d", &matrix[i][j]);
+      scanf("%d", matrix[i] + j);
     }
   }
 
-  for (int i = 0; i < rows; i++)
+  for (int i = 0; i < matrix[0][2] + 1; i++)
   {
-    for (int j = 1; j < rows - i; j++)
+    for (int j = 1; j < matrix[0][2] + 1 - i; j++)
     {
       if (j - 1 != 0 && matrix[j][1] < matrix[j - 1][1])
       {
@@ -32,13 +34,13 @@ void main()
         }
       }
     }
-    temp = matrix[rows - i - 1][0];
-    matrix[rows - i - 1][0] = matrix[rows - i - 1][1];
-    matrix[rows - i - 1][1] = temp;
+    temp = matrix[matrix[0][2] - i][0];
+    matrix[matrix[0][2] - i][0] = matrix[matrix[0][2] - i][1];
+    matrix[matrix[0][2] - i][1] = temp;
   }
 
   printf("The transpose of the given sparse matrix is:\n");
-  for (int i = 0; i < rows; i++)
+  for (int i = 0; i < matrix[0][2] + 1; i++)
   {
     for (int j = 0; j < 3; j++)
     {
---
+++ 2nd Year/Day - 3/Question - 2.c
@@ -0,0 +1,66 @@
+// WAP to perform addition of two given sparse matrix in 3–tuple format.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+  int ***matrix = (int ***)malloc(2 * sizeof(int **));
+  for (int k = 0; k < 2; k++)
+  {
+    matrix[k] = (int **)malloc(sizeof(int *));
+    matrix[k][0] = (int *)malloc(3 * sizeof(int));
+    printf("Enter sparse matrix - %d in 3-tuple format: \n", k + 1);
+    for (int i = 0; i < 3; i++)
+      scanf("%d", matrix[k][0] + i);
+    matrix[k] = (int **)realloc(matrix[k], (matrix[k][0][2] + 1) * sizeof(int *));
+    for (int i = 1; i <= matrix[k][0][2]; i++)
+    {
+      matrix[k][i] = (int *)malloc(3 * sizeof(int));
+      for (int j = 0; j < 3; j++)
+        scanf("%d", matrix[k][i] + j);
+    }
+  }
+
+  int **result = (int **)malloc(sizeof(int *)), count = 0, index = 1;
+  result[0] = (int *)malloc(3 * sizeof(int));
+  result[0][0] = matrix[0][0][0];
+  result[0][1] = matrix[0][0][1];
+  int largerMatrix = matrix[0][matrix[0][0][2]][0] >= matrix[1][matrix[1][0][2]][0] && matrix[0][matrix[0][0][2]][1] > matrix[1][matrix[1][0][2]][1] ? 0 : 1;
+  int smallerMatrix = largerMatrix == 0 ? 1 : 0;
+  for (int i = 1; i <= matrix[largerMatrix][0][2]; i++)
+  {
+    int value = matrix[largerMatrix][i][2];
+    for (int j = index; j <= matrix[smallerMatrix][0][2]; j++)
+    {
+      if (matrix[smallerMatrix][j][0] <= matrix[largerMatrix][i][0] && matrix[smallerMatrix][j][1] < matrix[largerMatrix][i][1])
+      {
+        result = (int **)realloc(result, (++count + 1) * sizeof(int *));
+        result[count] = (int *)malloc(3 * sizeof(int));
+        result[count][0] = matrix[smallerMatrix][j][0];
+        result[count][1] = matrix[smallerMatrix][j][1];
+        result[count][2] = matrix[smallerMatrix][j][2];
+        index = j + 1;
+      }
+      else if (matrix[smallerMatrix][j][0] == matrix[largerMatrix][i][0] && matrix[smallerMatrix][j][1] == matrix[largerMatrix][i][1])
+      {
+        value += matrix[smallerMatrix][j][2];
+        index = j + 1;
+      }
+    }
+    result = (int **)realloc(result, (++count + 1) * sizeof(int *));
+    result[count] = (int *)malloc(3 * sizeof(int));
+    result[count][0] = matrix[largerMatrix][i][0];
+    result[count][1] = matrix[largerMatrix][i][1];
+    result[count][2] = value;
+  }
+  result[0][2] = count;
+
+  printf("Resultant Matrix in 3-tuple format:\n");
+  for (int i = 0; i <= result[0][2]; i++)
+  {
+    for (int j = 0; j < 3; j++)
+      printf("%d ", result[i][j]);
+    printf("\n");
+  }
+}
