2bb13e947432bf3efade0a26f96ad98b67170138
VanXodus305 <soumyadeep.30505@gmail.com>
2024-10-11 19:22:13 +0530

    Updated Day - 7: Question - 1

--- 2nd Year/Day - 7/Question - 1.c
+++ 2nd Year/Day - 7/Question - 1.c
@@ -4,16 +4,15 @@
 #include <stdio.h>
 
 int front = -1, rear = -1;
-int queue[100];
 
-void enqueue(int value)
+void enqueue(int value, int queue[], int size)
 {
   if (rear == -1 && front == -1)
   {
     front = 0;
     rear = 0;
   }
-  else if (rear == 100 - 1)
+  else if (rear == size - 1)
   {
     printf("Queue is Full.\n");
     return;
@@ -25,7 +24,7 @@ void enqueue(int value)
   queue[rear] = value;
 }
 
-int dequeue()
+int dequeue(int queue[], int size)
 {
   if (front == -1 && rear == -1)
   {
@@ -45,7 +44,7 @@ int dequeue()
   }
 }
 
-void print()
+void print(int queue[], int size)
 {
   if (front == -1 && rear == -1)
   {
@@ -59,7 +58,7 @@ void print()
   printf("\n");
 }
 
-void isEmpty()
+void isEmpty(int queue[], int size)
 {
   if (front == -1 && rear == -1)
   {
@@ -71,9 +70,9 @@ void isEmpty()
   }
 }
 
-void isFull()
+void isFull(int queue[], int size)
 {
-  if (rear == 100 - 1)
+  if (rear == size - 1)
   {
     printf("Queue is Full.\n");
   }
@@ -85,7 +84,10 @@ void isFull()
 
 void main()
 {
-  int choice;
+  int choice, size;
+  printf("Enter the size of the Queue: ");
+  scanf("%d", &size);
+  int queue[size];
   printf("Main Menu:\n1. Enqueue\n2. Dequeue\n3. IsEmpty\n4. IsFull\n5. Traverse\n6. Exit\n");
   while (choice != 6)
   {
@@ -97,20 +99,20 @@ void main()
       printf("Enter the value to be enqueued: ");
       int value;
       scanf("%d", &value);
-      enqueue(value);
+      enqueue(value, queue, size);
       break;
     case 2:
-      printf("%d deleted from the Queue.\n", dequeue());
+      printf("%d deleted from the Queue.\n", dequeue(queue, size));
       break;
     case 3:
-      isEmpty();
+      isEmpty(queue, size);
       break;
     case 4:
-      isFull();
+      isFull(queue, size);
       break;
     case 5:
       printf("Queue: ");
-      print();
+      print(queue, size);
       break;
     case 6:
       break;
